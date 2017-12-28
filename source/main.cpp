b26aa85f509d89f01ce3ef5c6497bf4b35434aa8
Kirk Lange <kirklange@pm.me>
2017-12-27 20:08:02 -0800

    Hello world

---
+++ source/main.cpp
@@ -0,0 +1,19 @@
+/* main.cpp
+Copyright (c) 2018 by Kirk Lange
+
+Main function for ezsdl2. None of this code is part of the engine itself.
+Rather, treat this as an example or template for basing your future projects
+off of.
+*/
+
+//#include ezsdl2/
+#include <cstdio>
+
+
+
+int main(int argc, char *argv[])
+{
+    printf("Hello world, I am ezsdl2!\n");
+
+    return 0;
+}
