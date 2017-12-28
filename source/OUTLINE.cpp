69ab289919e92bc8f3b62a4e0ab2a95b860d5047
Kirk Lange <mail@kirklange.com>
2017-12-27 20:07:46 -0800

    Class outlines to be copied and renamed

---
+++ source/OUTLINE.cpp
@@ -0,0 +1,34 @@
+/* OUTLINE.hpp
+Copyright (c) 2018 by Kirk Lange
+
+TODO: high-level description
+*/
+
+#ifndef DEFINEME_HPP
+#define DEFINEME_HPP
+
+#include "STUFF.hpp"
+
+class STUFF;
+
+
+
+class OUTLINE
+{
+public:
+    // ctor: TODO description
+    OUTLINE();
+    
+    // dtor: TODO description
+    virtual ~OUTLINE();
+
+
+protected:
+
+
+private:
+};
+
+
+
+#endif // DEFINEME_HPP
---
+++ source/OUTLINE.hpp
@@ -0,0 +1,20 @@
+/* OUTLINE.cpp
+Copyright (c) 2018 by Kirk Lange
+
+TODO: brief implementation summary
+*/
+
+#include "OUTLINE.hpp"
+#include "STUFF.hpp"
+
+
+
+OUTLINE::OUTLINE()
+{
+    // ctor
+}
+
+OUTLINE::~OUTLINE()
+{
+    // dtor
+}
