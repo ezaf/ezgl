491416eedfb892effacaf4865e3e2097cff344ab
Kirk Lange <mail@kirklange.com>
2017-12-27 23:35:18 -0800

    Added (c) in source files. OUTLINE.cpp and hpp were swapped

--- source/OUTLINE.hpp
+++ source/OUTLINE.hpp
@@ -1,20 +1,43 @@
-/* OUTLINE.cpp
+/* OUTLINE.hpp
 Copyright (c) 2018 by Kirk Lange
 
-TODO: brief implementation summary
+TODO: high-level description
+
+EZSDL is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+EZSDL is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 */
 
-#include "OUTLINE.hpp"
+#ifndef DEFINEME_HPP
+#define DEFINEME_HPP
+
 #include "STUFF.hpp"
 
+class STUFF;
 
 
-OUTLINE::OUTLINE()
+
+class OUTLINE
 {
-    // ctor
-}
+public:
+    // ctor: TODO description
+    OUTLINE();
+    
+    // dtor: TODO description
+    virtual ~OUTLINE();
 
-OUTLINE::~OUTLINE()
-{
-    // dtor
-}
+
+protected:
+
+
+private:
+};
+
+
+
+#endif // DEFINEME_HPP
