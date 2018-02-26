12831cd0e76850a51041f4806613515b63f55a91
Kirk Lange <mail@kirklange.com>
2018-02-26 00:18:11 -0800

    Added input handler + macro for getter function generation

--- source/ezwindow.c
+++ source/ezwindow.c
@@ -31,9 +31,10 @@ struct ezwindow
 {
     SDL_Window      *mnWindow;
     SDL_Renderer    *mnRenderer;
+    SDL_Event       *mnEvent;
     SDL_DisplayMode  mDisplayMode;
-    SDL_Event        mEvent;
     uint8_t          mIsRunning;
+    uint8_t          mIsPaused;
 };
 
 
@@ -50,7 +51,7 @@ ezwindow* ezwindow_new()
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
     
     rnSelf->mnWindow = SDL_CreateWindow( "ezsdl",
-            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
             640, 480,
             SDL_WINDOW_SHOWN );
 
@@ -59,7 +60,9 @@ ezwindow* ezwindow_new()
 
     SDL_SetRenderDrawColor(rnSelf->mnRenderer, 0x00, 0x00, 0xFF, 0xFF);
 
+    rnSelf->mnEvent = (SDL_Event*) malloc(sizeof(SDL_Event));
     rnSelf->mIsRunning = 1;
+    rnSelf->mIsPaused = 0;
 
 
     if (vError)
@@ -88,6 +91,7 @@ uint8_t ezwindow_del(ezwindow **pnfSelf)
         IMG_Quit();
         SDL_Quit();
 
+        free((*pnfSelf)->mnEvent);
         free(*pnfSelf);
         *pnfSelf = 0;
 
@@ -96,7 +100,7 @@ uint8_t ezwindow_del(ezwindow **pnfSelf)
     else
     {
         ezlog(VITAL, "ezwindow_del", "Skipped deletion of ezwindow instance. "
-                "Cannot free a null pointer. (Duh!)");
+                "Cannot free a null pointer.");
         return 0;
     }
 
@@ -104,49 +108,120 @@ uint8_t ezwindow_del(ezwindow **pnfSelf)
 
 
 
-SDL_Window* ezwindow_getWindow(ezwindow *pnSelf)
+/* C Preprocessor Magic ;)
+ * Typing out these getter functions gets pretty repetitive... */
+#define EZWINDOW_GETTER(get_t, fn, get_m, error) \
+    get_t ezwindow_##fn(ezwindow *pnSelf) \
+    { \
+        if (pnSelf) \
+        { \
+            return pnSelf->get_m; \
+        } \
+        else \
+        { \
+            ezlog(MAJOR, "ezwindow_"#fn, error); \
+            return 0; \
+        } \
+    }
+
+EZWINDOW_GETTER(SDL_Window*, getWindow, mnWindow, "Cannot get SDL_Window off "
+        "of an ezwindow null pointer.")
+
+EZWINDOW_GETTER(SDL_Renderer*, getRenderer, mnRenderer, "Cannot get SDL_"
+        "Renderer off of an ezwindow null pointer.")
+
+EZWINDOW_GETTER(SDL_Event*, getEvent, mnEvent, "Cannot get SDL_Event off of "
+        "an ezwindow null pointer.")
+
+EZWINDOW_GETTER(uint8_t, isRunning, mIsRunning, "Cannot determine if window "
+        "is running because an ezwindow null pointer was given.")
+
+EZWINDOW_GETTER(uint8_t, isPaused, mIsPaused, "Cannot determine if window is "
+        "paused because an ezwindow null pointer was given.")
+
+#undef EZWINDOW_GETTER
+
+
+
+uint8_t ezwindow_pollEvent(ezwindow *pnSelf)
 {
-    return pnSelf ? pnSelf->mnWindow : 0;
-}
-
-
-
-SDL_Renderer* ezwindow_getRenderer(ezwindow *pnSelf)
-{
-    return pnSelf ? pnSelf->mnRenderer : 0;
-}
-
-
-
-uint8_t ezwindow_isRunning(ezwindow *pnSelf)
-{
-    return pnSelf->mIsRunning;
-}
-
-
-
-void ezwindow_pollEvent(ezwindow *pnSelf)
-{
-    while ( SDL_PollEvent(&(pnSelf->mEvent)) )
+    uint8_t rPolling;
+    
+    if (pnSelf)
     {
-        if (pnSelf->mEvent.type == SDL_QUIT)
+        /* Save polling status so we can return to everyone else.
+         * It is essential that pnSelf's actual mnEvent rather than a copy
+         *   of pnSelf *mnEvent be passed to SDL_PollEvent() */
+        rPolling = SDL_PollEvent(ezwindow_getEvent(pnSelf));
+        /* So that I don't have to keep typing this darn function call */
+        SDL_Event vEvent = *ezwindow_getEvent(pnSelf);
+
+        /* Internal event handling */
+        if (rPolling)
         {
-            pnSelf->mIsRunning = 0;
+            switch (vEvent.type)
+            {
+                case SDL_QUIT:
+                    pnSelf->mIsRunning = 0;
+                    break;
+
+                case SDL_KEYDOWN:
+                    switch (vEvent.key.keysym.scancode)
+                    {
+                        case SDL_SCANCODE_ESCAPE:
+                            pnSelf->mIsRunning = 0;
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                case SDL_KEYUP:
+                    break;
+
+                case SDL_WINDOWEVENT:
+                    switch (vEvent.window.event)
+                    {
+                        /* Pause app if lost keyboard focus (like tab out) */
+                        case SDL_WINDOWEVENT_FOCUS_GAINED:
+                            pnSelf->mIsPaused = 0;
+                            break;
+                        case SDL_WINDOWEVENT_FOCUS_LOST:
+                            pnSelf->mIsPaused = 1;
+                            break;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
+    else
+    {
+        ezlog(MAJOR, "ezwindow_pollEvent", "Cannot poll events off of an "
+                "ezwindow null pointer.");
+        rPolling = 0;
+    }
+    
+    /* Return polling status to everyone else */
+    return rPolling;
 }
 
 
 
 void ezwindow_clear(ezwindow *pnSelf)
 {
+    /* TODO: For now it's an obnoxious, bright blue so that it's obvious
+     * when the bare background is exposed. Replace later with black. */
     SDL_SetRenderDrawColor(pnSelf->mnRenderer, 0x00, 0x00, 0xFF, 0xFF);
     SDL_RenderClear(pnSelf->mnRenderer);
 }
 
 
 
-void ezwindow_update(ezwindow *pnSelf)
+void ezwindow_render(ezwindow *pnSelf)
 {
     SDL_RenderPresent(pnSelf->mnRenderer);
 }
