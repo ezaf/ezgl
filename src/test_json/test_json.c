326243ab827d33d58c5f772a6df00ab0b7c53c24
Kirk Lange <kirklange@pm.me>
2018-08-06 00:04:22 -0700

    Fixed undef ref to SDL_main error on msys2

--- src/test_json/test_json.c
+++ src/test_json/test_json.c
@@ -1,15 +1,15 @@
 /*  test_json.c
- *  
+ *
  *  Copyright (c) 2018 Kirk Lange <github.com/kirklange>
- *  
+ *
  *  This software is provided 'as-is', without any express or implied
  *  warranty. In no event will the authors be held liable for any damages
  *  arising from the use of this software.
- *  
+ *
  *  Permission is granted to anyone to use this software for any purpose,
  *  including commercial applications, and to alter it and redistribute it
  *  freely, subject to the following restrictions:
- *  
+ *
  *  1. The origin of this software must not be misrepresented; you must not
  *     claim that you wrote the original software. If you use this software
  *     in a product, an acknowledgment in the product documentation would be
@@ -25,6 +25,9 @@
  */
 
 #include "nlohmann/json.hpp"
+
+// Prevents "undefined reference to 'SDL_main'" error
+#include <SDL2/SDL.h>
 
 #include <fstream>
 #include <iostream>
